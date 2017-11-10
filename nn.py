057ff2477804d83388af1ac802ce1c46d06bd817
synthmed <tejpalv8@gmail.com>
2017-11-10 13:09:22 -0800

    added sigmoid

--- nn.py
+++ nn.py
@@ -1,28 +1,36 @@
 import torch
+import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
+import math
 
 dtype = torch.FloatTensor
+
+def sigmoid(x):
+  return 1/(1 + math.e ** -x)
 
 d_in = 3
 hidden1 = 5
 hidden2 = 5
 d_out = 1
-lr = 0.00001
+lr = 0.001
 epochs = 100
 
-a = np.array([[5,1,10], [8,3,4], [9,7,1], [5,2,8], [8,3,4]])
+a = np.array([[5,1,10], [8,3,4], [9,7,1], [5,2,8], [8,4,3]])
 x = Variable(torch.from_numpy(a).type(dtype), requires_grad = False)
 
-b = np.array([10,8,9,8,8])
+b = np.array([50,96,63,80,96])
 y = Variable(torch.from_numpy(b).type(dtype), requires_grad = False)
 
 w1 = Variable(torch.randn(d_in, hidden1).type(dtype), requires_grad = True)
 w2 = Variable(torch.randn(hidden1, hidden2).type(dtype), requires_grad = True)
 w3 = Variable(torch.randn(hidden2, d_out).type(dtype), requires_grad = True)
 
+
 for i in range(epochs):
-  output = x.mm(w1).mm(w2).mm(w3)
+  output = sigmoid(x.mm(w1))
+  output = sigmoid(output.mm(w2))
+  output = sigmoid(output.mm(w3))
   
   # mean squared loss:
   loss = (output - y).pow(2).sum()
@@ -39,4 +47,4 @@ for i in range(epochs):
   w2.grad.data.zero_()
   w3.grad.data.zero_()
 
-print(output)
+print(x.mm(w1).mm(w2).mm(w3))
